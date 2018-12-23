2957d883bc2dfdf9aed1528ae11935df6d434130
Osipov Nikolay <justdoit251996@gmail.com>
2018-12-23 23:45:46 +0300

    Add files via upload

---
+++ script.sh
@@ -0,0 +1,13 @@
+dieharder -d 0 -f to_test2.bin
+dieharder -d 2 -f to_test2.bin
+dieharder -d 3 -f to_test2.bin
+dieharder -d 204 -f to_test2.bin
+dieharder -d 15 -f to_test2.bin
+dieharder -d 12 -f to_test2.bin
+dieharder -d 11 -f to_test2.bin
+dieharder -d 16 -f to_test2.bin
+dieharder -d 13 -f to_test2.bin
+dieharder -d 14 -f to_test2.bin
+dieharder -d 10 -f to_test2.bin
+
+
